8b60c23425c84db12258b01a866090afdb26ad3e
Hetbhalodiya91 <bhalodiyahet9126@gmail.com>
2026-02-12 21:35:57 +0530

    room crud

---
+++ backend/src/main/java/com/chat/backend/entity/Message.java
@@ -0,0 +1,27 @@
+package com.chat.backend.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    private String sender;
+    private String content;
+    private LocalDateTime timeStamp;
+
+    public Message(String sender, String content) {
+        this.sender = sender;
+        this.content = content;
+        this.timeStamp = LocalDateTime.now();
+    }
+}
