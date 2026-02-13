a00daf522de422c95acb71a0e4eac3c38707aa6c
Hetbhalodiya91 <bhalodiyahet9126@gmail.com>
2026-02-13 09:47:07 +0530

    feat(chat-backend): complete real-time messaging setup
    - Configure WebSocket
    - Add ChatController for message broadcasting
    - Add RoomController for room handling
    - Create MessageRequest model

--- backend/src/main/java/com/chat/backend/controller/ChatController.java
+++ backend/src/main/java/com/chat/backend/controller/ChatController.java
@@ -1,9 +1,43 @@
 package com.chat.backend.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.chat.backend.Playload.MessageRequest;
+import com.chat.backend.entity.Message;
+import com.chat.backend.entity.Room;
+import com.chat.backend.repository.RoomRepository;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
-@RequestMapping("")
+import java.time.LocalDateTime;
+
+@Controller
+@CrossOrigin("http://localhost:3000")
 public class ChatController {
+    private RoomRepository roomRepository;
+
+    public ChatController(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
+    }
+
+    @MessageMapping("/sendMessage/{roomId}")
+    @SendTo("/topic/room/{roomId}")
+    public Message sendMessage(
+            @DestinationVariable String roomId
+            ,@RequestBody MessageRequest messageRequest){
+
+        Room room = roomRepository.findByRoomId(messageRequest.getRoomId());
+        Message message = new Message();
+        message.setContent(messageRequest.getContent());
+        message.setSender(messageRequest.getSender());
+        message.setTimeStamp(LocalDateTime.now());
+        if(room != null){
+            room.getMessages().add(message);
+            roomRepository.save(room);
+        }else{
+            throw new RuntimeException("room not found!");
+        }
+        return message;
+    }
 }
